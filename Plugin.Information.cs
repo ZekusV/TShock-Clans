5dc4ab2f295e0fdd33eec4fb44e7469250f0c6ec
Zeke <112866998+ZekusV@users.noreply.github.com>
2023-06-27 21:42:17 +0100

    Update Plugin.Information.cs

--- Plugin.Information.cs
+++ Plugin.Information.cs
@@ -1,17 +1,19 @@
-﻿using Terraria;
-using TerrariaApi.Server;
+using System;
+using Terraria;
+using TShockAPI;
 
-namespace PluginTemplate
+namespace ClanPlugin
 {
-    public partial class Plugin: TerrariaPlugin
+    public static class PluginInformation
     {
-        public override string Name => "PluginTemplate";
-        public override Version Version => new(0, 0, 1);
-        public override string Author => "Dekay";
-        public override string Description => "Template to create new Tshock Plugins";
+        public static readonly string Author = "Zekevious";
+        public static readonly string Description = "Clan Plugin";
+        public static readonly string Name = "ClanPlugin";
+        public static readonly Version Version = new Version(1, 0, 0);
 
-        public Plugin(Main game) : base(game)
+        public static void Initialize()
         {
+            TShock.Log.ConsoleInfo($"Loading {Name} v{Version} by {Author}");
         }
     }
 }
